e7b141a20c3f87e33724a84bb080951a0d7dfa8d
Devan <devanputradanendra@gmail.com>
2026-09-16 13:25:29 +0700

    fix: correct versionCode 8 and versionName 2.3 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "id.nusantara.cctv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.2"
+        versionCode = 8
+        versionName = "2.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField(
